baa02b469dfb2d8fb2f8a268bfd953dc83dc7c97
Rusian <rusianxairr@gmail.com>
2026-05-18 09:35:51 -0400

    Refactor PostulanteFeignClient interface comments

--- empresa (3)/empresa/empresa/src/main/java/com/empresa/empresa/client/PostulanteFeignClient.java
+++ empresa (3)/empresa/empresa/src/main/java/com/empresa/empresa/client/PostulanteFeignClient.java
@@ -4,14 +4,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-// El "name" debe ser exactamente el nombre con el que POSTULANTE se registró en Eureka
+
 @FeignClient(name="postulante") 
 public interface PostulanteFeignClient {
 
-    // Aquí pones la ruta exacta que usarías para buscar un postulante en el otro microservicio
-    // (Asegúrate de que esta ruta exista en el controlador de postulante)
+   
     @GetMapping("/api/v1/postulantes/{id}")
     Object getPostulanteById(@PathVariable("id") Long id); 
-    
-    // Nota: Reemplaza "Object" por tu PostulanteDTO si tienes uno creado.
 }
